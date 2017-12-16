2a26e651b17acb821f711390cbe3e7d66a7ac3c1
mamen <markusme@edu.aau.at>
2017-12-16 11:29:15 +0100

    updated playermovement + basic leveldesign

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -13,9 +13,10 @@ public class playerMovement : MonoBehaviour {
 	private float targetTime = 2.0f;  // max keypress time
 	private float currentTime = 0.0f; // already pressed time
 
-	private float speed = 6;
+	private int hSpeed = 0;
+	public int hSpeedMax = 6;
 
-	private float vSpeed = 2;
+	public float vSpeed = 2;
 
 	private float vSpeedDefault = 2;
 	private float vSpeedMax = 8;
@@ -31,9 +32,11 @@ public class playerMovement : MonoBehaviour {
 	void Update () {
 
 		if (Input.GetKey(moveLeft)) {
-//			rigidbody2D.velocity = new Vector2(speed *-1, vSpeed);
+			// change horizontal direction
+			hSpeed = -hSpeedMax;
 		} else if (Input.GetKey(moveRight)) {
-//			rigidbody2D.velocity = new Vector2(speed, vSpeed);
+			// change horizontal direction
+			hSpeed = hSpeedMax;
 		} else if(Input.GetKey(moveDown)) {
 
 			currentTime += Time.deltaTime;
@@ -47,7 +50,7 @@ public class playerMovement : MonoBehaviour {
 
 //			vSpeed = -8+(1 + (1 / (Mathf.Ceil(currentTime))));
 
-			rigidbody2D.velocity = new Vector2(0, vSpeed*-1f);
+//			rigidbody2D.velocity = new Vector2(hSpeed, vSpeed*-1f);
 
 
 		} else if(Input.GetKey(moveUp)) {
@@ -59,10 +62,15 @@ public class playerMovement : MonoBehaviour {
 			rigidbody2D.velocity = new Vector2(0, 0);
 			currentTime = 0;
 		} else {
-//			rigidbody2D.velocity = new Vector2(0, vSpeed);
+			// no button is pressed
+			hSpeed = 0;
+			vSpeed = vSpeedDefault;
 		}
 
-//		rigidbody2D.velocity = new Vector2(0, 0);
+
+		print (hSpeed + " - " + vSpeed);
+
+		rigidbody2D.velocity = new Vector2(hSpeed, vSpeed*-1);
 
 
 	}
