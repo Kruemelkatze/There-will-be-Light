ec479583fe6a7b5ece07b9f023fcf219d40dda6c
Kruemelkatze <schoberf@gmx.net>
2017-12-16 12:56:43 +0100

    Simple Character prefab

--- Assets/Scripts/SpeedPrinter.cs
+++ Assets/Scripts/SpeedPrinter.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpeedPrinter : MonoBehaviour {
+public class SpeedPrinter : MonoBehaviour
+{
 
     public Text text;
+    public bool Enabled = true;
 
     private Rigidbody2D rigid;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rigid = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        text.text = $"{rigid.velocity.x} {rigid.velocity.y}";
-	}
+        text.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Enabled)
+        {
+            text.text = $"{rigid.velocity.x} {rigid.velocity.y}";
+        } else
+        {
+            text.text = "";
+        }
+    }
 }
