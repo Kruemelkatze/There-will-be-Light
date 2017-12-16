cb5e05a9df68f9d9c07d30a435b403b3dbd201c4
Kruemelkatze <schoberf@gmx.net>
2017-12-16 23:51:40 +0100

    new Character Light

---
+++ Assets/Scripts/CharLight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharLight : MonoBehaviour
+{
+
+    public GameObject Red;
+    public GameObject Yellow;
+    public GameObject Blue;
+
+    // Use this for initialization
+    void Start()
+    {
+        Hub.Get<EventHub>().PlayerColorChanged += ColorChanged;
+    }
+
+    private void ColorChanged()
+    {
+        Red.SetActive(false);
+        Blue.SetActive(false);
+        Yellow.SetActive(false);
+
+        switch (Hub.Get<GameManager>().CurrentColor)
+        {
+            case GameManager.PlayerColor.Red:
+                Red.SetActive(true);
+                break;
+            case GameManager.PlayerColor.Blue:
+                Blue.SetActive(true);
+                break;
+            case GameManager.PlayerColor.Yellow:
+                Yellow.SetActive(true);
+                break;
+        }
+    }
+}
