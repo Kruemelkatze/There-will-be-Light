87ab4452ee9da3167c28cc211642eed0a470b768
Tarek <t.abdel-aziz@gmx.at>
2017-12-16 13:35:26 +0100

    Splash respawn

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject checkpoint;
+    
+
+	// Use this for initialization
+	void Start () {
+        generateLevel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void generateLevel(){
+        generateCheckpoint();
+    }
+
+    void generateCheckpoint()
+    {
+        Instantiate(checkpoint, new Vector3(0.25f, 4, 0), Quaternion.identity);
+    }
+
+}
---
+++ Assets/Scripts/playerCollison.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollison : MonoBehaviour
+{
+
+    public GameObject explosion;
+    public GameObject implosion;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Start");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Enter " + other.tag);
+        if (other.gameObject.tag.Equals("red"))
+        {
+            Instantiate(explosion, this.transform.position, Quaternion.identity);
+            this.transform.position = GameObject.FindGameObjectWithTag("checkpoint").transform.position;
+        }
+    }
+}
